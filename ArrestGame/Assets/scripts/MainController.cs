b89316bc8048d63f80383a5f1db46c3021bea4b3
nate <natelow@rocketmail.com>
2018-05-30 19:01:16 -0700

    MainController and TextReader
    Set up the basic layout for the text reader, and the initial variables needed for the main controller

---
+++ ArrestGame/Assets/scripts/MainController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VNFramework;
+
+public class MainController : MonoBehaviour {
+
+    public GameObject room;
+    public GameObject character;
+    public GameObject evidence;
+
+    public TextAsset roomstxt;
+    public TextAsset characterstxt;
+    public TextAsset evidencetxt;
+
+    private List<Room> roomList = new List<Room>();
+    private List<Character> characterList = new List<Character>();
+    private List<Evidence> evidenceList = new List<Evidence>();
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ArrestGame/Assets/scripts/TextReader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VNFramework;
+
+
+public class TextReader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public List<Room> readinRooms(TextAsset t) {
+
+        List<Room> r = new List<Room>();
+
+
+        return r;
+    }
+
+    public List<Character> readinCharacters(TextAsset t) {
+
+        List<Character> c = new List<Character>();
+
+
+        return c;
+    }
+
+    public List<Evidence> readinEvidence(TextAsset t) {
+
+        List<Evidence> e = new List<Evidence>();
+
+
+        return e;
+    }
+
+}
