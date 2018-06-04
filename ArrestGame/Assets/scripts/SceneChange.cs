47d2c6cb6677ef70a06eaa1cf3010fcecec0407b
nate <natelow@rocketmail.com>
2018-06-03 20:22:08 -0700

    Scene Changing
    Added scene changing and a plain menu scene.

---
+++ ArrestGame/Assets/scripts/SceneChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void changeScene (string s) {
+        if (s == "Settings") {
+            SceneManager.LoadScene(s, LoadSceneMode.Additive);
+        } else {
+            SceneManager.LoadScene(s);
+        }
+        
+    }
+
+}
